f0a15c5871ed4a98f6539ae6f471c73c267e0f24
Seven Yu <dofyyu@gmail.com>
2016-10-24 15:18:02 +0800

    添加注释

--- index.js
+++ index.js
@@ -13,10 +13,13 @@ app.set('view engine', 'ejs');
 
 app.use('/', require('./lib/routers/demo'));
 
+// 连接数据库
 mongo.connect('mongodb://localhost/demo', function(err) {
     if(err) {
+        // 连接失败
         console.log(err);
     } else {
+        // 连接成功
         app.listen(3000, function(err, b) {
             console.log('running...');
         })
